9c4c6ffcb98ef1c85dc7d21aa1bac2bb9300b381
p-lau <plau@wpi.edu>
2020-03-31 19:39:58 -0400

    redirects

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,17 +1,16 @@
 import React, {useEffect, useState} from "react";
 import Markdown from "react-markdown";
 import {useParams} from "react-router";
-import '../blogs/team-document.md';
 
 const Blog = () => {
     const { blog } = useParams();
     const [markdown, setMarkdown] = useState('');
 
     useEffect(()=>{
-
         if(!markdown){
             async function fetchBlog (){
-                await fetch('https://raw.githubusercontent.com/p-lau/rhythm-game/master/src/blogs/team-document.md').then(r => {return r.text()}).then(text =>  setMarkdown(text));
+                let blogPath = require(`../blogs/${blog}.md`);
+                await fetch(blogPath).then(r => {return r.text()}).then(text =>  setMarkdown(text));
             }
             fetchBlog().then(r => null);
         }
