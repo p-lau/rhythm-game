d40bc711e00a21c4425a4f58a3cd642d93bf1b99
p-lau <plau@wpi.edu>
2020-04-03 15:00:55 -0400

    add files for parallax

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@
 export {default as Navbar} from './navbar';
 export {default as Page} from './routes';
 export {default as Footer} from './footer';
+export {default as Parallax} from './parallax'
 export {PROFILES, POSTS} from './constants';
---
+++ src/components/parallax.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Parallax = props => {
+  return(
+      <section className={'parallax'}>
+          <img id={'key-art1'} alt={'key-art1'}/>
+          <img id={'key-art2'} alt={'key-art2'}/>
+          <img id={'key-art3'} alt={'key-art3'}/>
+          <img id={'key-art4'} alt={'key-art4'}/>
+      </section>
+  )
+};
+
+export default Parallax;
