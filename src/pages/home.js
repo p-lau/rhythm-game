6d43f0476d2de02eb7f0861db5ed6413a7c6a2eb
p-lau <plau@wpi.edu>
2020-04-22 14:17:05 -0400

    uoptrshterhsheshtes

--- src/pages/home.js
+++ src/pages/home.js
@@ -35,8 +35,10 @@ const Home = () => {
             <header><h1>Rhythm of the Night</h1></header>
             <main>
                 <SEO title={'Home'} description={'A hack-n-bash rhythm game from Team 2/4, a group from IMGD4000/4500 course of WPI'}/>
-                <h1>We're currently working on this game</h1>
-                <p>Check back soon for the Alpha release!</p>
+                <h1>Alpha Release</h1>
+                <p>Enjoy the game!</p>
+                <a download className={'button'} href={"https://drive.google.com/open?id=1JaERjOnAkKfV_98YlqsZYzcsSwVM3IUY"}>Download</a>
+                <a className={"button"} href={"https://docs.google.com/presentation/d/1D_J4EvH2JdCv9HRu4FSRkvJzQSQqw8uqj_JtRzRrw0o/edit?usp=sharing"}>Presentation</a>
             </main>
         </div>
     )
