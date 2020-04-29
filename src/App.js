4eb79b765e21129a8405f846da16db319d8f2058
p-lau <plau@wpi.edu>
2020-04-29 15:46:17 -0400

    Playtesting update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {Navbar, Page, Footer} from './components';
 import './styles/App.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faCheckSquare, faCoffee, faMouse } from '@fortawesome/free-solid-svg-icons'
 import {ParallaxProvider} from "react-scroll-parallax";
 
-library.add(fab, faCheckSquare, faCoffee);
+library.add(fab, faCheckSquare, faCoffee, faMouse);
 
 
 const App = () => {
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,8 @@ import React from "react";
 import {ParallaxBanner} from "react-scroll-parallax";
 import '../styles/home.css'
 import {SEO} from "../components";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMouse} from "@fortawesome/free-solid-svg-icons/faMouse";
 
 const Home = () => {
     return (
@@ -29,30 +31,37 @@ const Home = () => {
                     },
                 ]}
                 style={{
-                    height: '45rem',
+                    height: '80vh',
                 }}
             />
             <header><h1>Rhythm of the Night</h1></header>
             <main>
                 <SEO title={'Home'} description={'A hack-n-bash rhythm game from Team 2/4, a group from IMGD4000/4500 course of WPI'}/>
                 <div>
-                    <h1>Alpha Release</h1>
+                    <h1>Playtesting</h1>
                     <hr/>
-                    <h2>Presentation</h2>
-                    <iframe
-                        title="Alpha Presentation"
-                        src="https://docs.google.com/presentation/d/e/2PACX-1vRH6uhQqMhx5nACSC8pabbxqvGXw19YnnHyifheaaeLQGotm0AwdBXJH_Xwdu162Eb7KDpn58KlWHY0/embed?start=false&loop=false&delayms=3000"
-                        frameBorder="0" width="100%" height="360" allowFullScreen="true" mozallowfullscreen="true"
-                        webkitallowfullscreen="true"/>
-                    <h2>Demo</h2>
-                    <iframe
-                        title="Alpha Demo"
-                        width="100%" height="360" src="https://www.youtube-nocookie.com/embed/EKGr-U7v1xA"
-                        frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen/>
-                    <a download className={'button'} href={"https://drive.google.com/open?id=1JaERjOnAkKfV_98YlqsZYzcsSwVM3IUY"}>Download the Alpha</a>
+                    <h2>We're looking for people to test our game!</h2>
+                    <h3>Directions:</h3>
+                    <p>Download and extract the .zip file from the download link</p>
+                    <p>Run the .exe file after extraction</p>
+                    <p>Click [Play] to start the game</p>
+                    <p>Defeat the snakes while attacking by the music's rhythm. A metronome displayed as a visual guide for the music rhythm</p>
+                    <p>The game ends once the player clears three waves of enemy snakes or the tent's or character's health bar drops to 0 (displayed at the sides)</p>
+                    <p>Press the Esc key to bring up the menu and click [Quit] to close and exit the game</p>
+                    <h4>Controls:</h4>
+                    <p>[Esc] to go to menu screen</p>
+                    <p>[WASD] to move</p>
+                    <p>[Left-mouse button]  to attack</p>
+                    <p>[Spacebar] to attack</p>
+                    <p>Move the cursor with the mouse <FontAwesomeIcon icon={faMouse} /></p>
+                    <p>The character will face in the direction towards the cursor</p>
 
+                    <a download className={'button'} href={"https://drive.google.com/open?id=1JaERjOnAkKfV_98YlqsZYzcsSwVM3IUY"}>Download the Game</a>
+                    <a className={'button'} href={"https://forms.gle/P9trsCRfUKfkp3mT7"}>Take the survey</a>
+                    <p>Currently for Windows <FontAwesomeIcon icon={['fab', 'windows']} /> only</p>
+                    <p> </p>
                     <p>Enjoy the game!</p>
+                    <p> </p>
                 </div>
             </main>
         </div>
